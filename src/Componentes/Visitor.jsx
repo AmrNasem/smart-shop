407f19cc7a31d5359993d6a3094d2f42a9991303
Mohammed Heggy <hegy395@gmail.com>
2024-01-21 22:14:20 +0200

    the same last commit with edit some styles

--- src/Componentes/Visitor.jsx
+++ src/Componentes/Visitor.jsx
@@ -6,21 +6,19 @@ import { Link } from 'react-router-dom'
 export const Visitor = () => {
   return (
     // remove the test div
-    <div className={visitor.test}>
-      <div className={visitor.container} dir='rtl'>
-        <div className={visitor.child1}>
-          <FontAwesomeIcon icon={faUser} />
-          <Link to="/login">
-            <p className={visitor.signinOp}>تسجيل الدخول</p>
-          </Link>
-        </div>
-        <div className={visitor.child2}>
-          <FontAwesomeIcon icon={faUser} />
-          <Link to="/signup">
-            <p className={visitor.signupOp}>تسجيل جديد</p>
-          </Link>
-        </div>
+    <div className={visitor.container} dir='rtl'>
+      <div className={visitor.child1}>
+        <FontAwesomeIcon icon={faUser} />
+        <Link to="/login">
+          <p className={visitor.signinOp}>تسجيل الدخول</p>
+        </Link>
       </div>
-    </div >
+      <div className={visitor.child2}>
+        <FontAwesomeIcon icon={faUser} />
+        <Link to="/signup">
+          <p className={visitor.signupOp}>تسجيل جديد</p>
+        </Link>
+      </div>
+    </div>
   )
 }
